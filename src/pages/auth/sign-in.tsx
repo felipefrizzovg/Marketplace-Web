e8490e9e75cb14d3db4d70c7a06f5ae3d3ea175e
Felipe Frizzo <felipefrizzovg@gmail.com>
2024-08-22 12:10:50 -0300

    Authenticating user

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,3 +1,4 @@
+import { useMutation } from '@tanstack/react-query'
 import { ArrowRight } from 'lucide-react'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
@@ -5,6 +6,7 @@ import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { signIn } from '@/api/sign-in'
 import { Button } from '@/components/ui/button'
 
 const signInForm = z.object({
@@ -21,14 +23,15 @@ export function SignIn() {
     formState: { isSubmitting },
   } = useForm<SignInForm>()
 
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+  })
+
   async function handleSignIn(data: SignInForm) {
     try {
-      console.log(data)
-
-      await new Promise((resolve) => setTimeout(resolve, 2000))
-
+      authenticate({ email: data.email, password: data.password })
       toast.success('Login realizado com sucesso')
-    } catch (e) {
+    } catch {
       toast.error('Credenciais inválidas')
     }
   }
