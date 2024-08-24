cabfb62de75bbd69b85cbd72dbf99352cab587ec
Felipe Frizzo <felipefrizzovg@gmail.com>
2024-08-24 11:06:16 -0300

    Sign-out function done

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,7 +1,9 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { LogOut } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 
 import { getProfile } from '@/api/get-profile'
+import { signOut } from '@/api/sign-out'
 import sofa from '@/assets/sofa.png'
 
 import {
@@ -14,9 +16,18 @@ import {
 } from './ui/dropdown-menu'
 
 export function AccountMenu() {
+  const navigate = useNavigate()
+
   const { data: profile } = useQuery({
     queryKey: ['profile'],
     queryFn: getProfile,
+  })
+
+  const { mutateAsync: signOutFn, isPending: isSigningOut } = useMutation({
+    mutationFn: signOut,
+    onSuccess: () => {
+      navigate('/sign-in', { replace: true })
+    },
   })
 
   return (
@@ -32,9 +43,15 @@ export function AccountMenu() {
           </div>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem className="flex items-center justify-between gap-2 text-orange-base">
-          Sair
-          <LogOut />
+        <DropdownMenuItem
+          asChild
+          className="flex items-center justify-between gap-2 text-orange-base"
+          disabled={isSigningOut}
+        >
+          <button className="w-full" onClick={() => signOutFn()}>
+            <span>Sair</span>
+            <LogOut />
+          </button>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
