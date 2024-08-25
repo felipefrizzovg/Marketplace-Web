4d124e5c91f852d11263cee9153bb4de1ded1b7d
Felipe Frizzo <felipefrizzovg@gmail.com>
2024-08-25 19:22:11 -0300

    Made profile image become variable to user

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -33,12 +33,16 @@ export function AccountMenu() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="flex h-12 w-12 items-center justify-center rounded-xl bg-shape-shape">
-        <img src={sofa} alt="" />
+        <img src={profile?.seller.avatar?.url || sofa} alt="" />
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuLabel>
           <div className="flex items-center gap-3">
-            <img className="h-8 w-8 rounded-md" src={sofa} alt="" />
+            <img
+              className="h-8 w-8 rounded-md"
+              src={profile?.seller.avatar?.url || sofa}
+              alt=""
+            />
             <span>{profile?.seller.name}</span>
           </div>
         </DropdownMenuLabel>
